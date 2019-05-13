508dbbeba863400e8b91c1c98b87341165c2b7d9
Nate Tan <nktan93@gmail.com>
2019-05-13 19:57:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
+    current_time = 
+    
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
