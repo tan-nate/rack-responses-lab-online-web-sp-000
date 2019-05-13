912eeafad90aea8af250b410c1ff7eff93d89406
Nate Tan <nktan93@gmail.com>
2019-05-13 20:01:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,27 +1,13 @@
-require 'date'
-require 'pry'
-
 class Application
  
   def call(env)
     resp = Rack::Response.new
  
-    current_time = Time.now
-    
-    binding.pry
-    
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
- 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+    current_hour = Time.now.hour
+    if current_hour < 12
+      resp.write "Good Morning!"
     else
-      resp.write "You Lose"
+      resp.write "Good Afternoon!"
     end
  
     resp.finish
